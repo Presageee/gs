30ca7be1358b11890223a2dd6ab24fb0c8429895
linjuntan <ljt1343@gmail.com>
2018-02-04 02:40:41 +0800

    add create object method, init service handler map

--- gs-common/src/main/java/com/gs/common/util/ReflectUtils.java
+++ gs-common/src/main/java/com/gs/common/util/ReflectUtils.java
@@ -1,12 +1,26 @@
 package com.gs.common.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Created by linjuntan on 2018/1/30.
  * email: ljt1343@gmail.com
  */
+@Slf4j
 public class ReflectUtils {
+
+    /**
+     * create object by class name
+     * @param clazzName class name
+     * @return new instance
+     */
     public static Object createObjectByClassName(String clazzName) {
-        //todo
+        try {
+            Class clazz = Class.forName(clazzName);
+            return clazz.newInstance();
+        } catch (Exception e) {
+            log.error(" >>> create {} instance error!", clazzName, e);
+        }
         return null;
     }
 }
